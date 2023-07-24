70d3ed4d6b799455eaa1adb3e9cb0a7ba6369239
Edwin <edwi.castruita117@gmail.com>
2023-07-24 12:44:50 -0600

    Se argega fondo de Hero y titulo inicial

---
+++ components/Hero.jsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <Head>
+        <title>ATC - Services</title>
+        <meta name="description" content="ATC - Services" />
+      </Head>
+      <div className="bgImage h-screen bg-cover bg-center ">
+        <article className="absolute top-[45%] left-[25%] md:top-[50%] md:left-[10%] lg:left-[20%]">
+          <Image
+            src="/img/xcien-logo-blanco.svg"
+            width={100}
+            height={100}
+            alt="Logo Xcien"
+            className="md:w-48"
+          />
+          <h1 className="text-white text-4xl md:text-8xl font-semibold mt-4">
+            ICT Services
+          </h1>
+        </article>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
